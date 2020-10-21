2b5a9697faf9821bec1b823a3e1226e628d604da
sarathbabu64 <48305138+sarathbabu64@users.noreply.github.com>
2020-10-20 20:05:49 -0500

    Update PhpAppTest.java

--- src/test/java/com/edu/PhpAppTest.java
+++ src/test/java/com/edu/PhpAppTest.java
@@ -11,7 +11,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class NewTest2 {
+public class PhpAppTest {
 	WebDriver driver;
 
 	@BeforeMethod
