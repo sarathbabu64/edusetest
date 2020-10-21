3d0ab16ddf2a5e756dbb635d750bcdccec3af3e0
sarathbabu64 <48305138+sarathbabu64@users.noreply.github.com>
2020-10-20 19:59:19 -0500

    Update NewTest2.java

--- src/test/java/com/edu/NewTest2.java
+++ src/test/java/com/edu/NewTest2.java
@@ -1,15 +1,15 @@
 package com.edu;
 
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeMethod;
-
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class NewTest2 {
 	WebDriver driver;
